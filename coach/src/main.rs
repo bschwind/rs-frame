b210e57bff2dde49e052b8a2287ffa5eace71642
Brian Schwind <brianmschwind@gmail.com>
2019-04-19 15:38:07 +0900

    API experimentation

--- coach/src/main.rs
+++ coach/src/main.rs
@@ -1,139 +1,205 @@
+use regex::Regex;
 use rs_frame::{App, Controller, RouteParams};
-use rs_frame_macros::{Controller, route};
+use rs_frame_macros::{route, AppPath};
 
 #[derive(Default, Hash)]
 struct EnvironmentDetailController {
-	env_id: String,
+    env_id: String,
 }
 
 impl Controller for EnvironmentDetailController {
-	fn params(&mut self, params: &RouteParams) -> Option<()> {
-		if params.data.name.starts_with("environments/") {
-			let env_id = params.data.name.rsplit("/").next().unwrap();
-			self.env_id = env_id.to_string();
+    fn params(&mut self, params: &RouteParams) -> Option<()> {
+        if params.data.name.starts_with("/environments/") {
+            let env_id = params.data.name.rsplit("/").next().unwrap();
+            self.env_id = env_id.to_string();
 
-			Some(())
-		} else {
-			None
-		}
-	}
+            Some(())
+        } else {
+            None
+        }
+    }
 
-	fn start(&self) -> Vec<String> {
-		println!("Environment detail controller starting with env_id: {}", self.env_id);
-		vec![format!("load env {}", self.env_id)]
-	}
+    fn start(&self) -> Vec<String> {
+        println!(
+            "Environment detail controller starting with env_id: {}",
+            self.env_id
+        );
+        vec![format!("load env {}", self.env_id)]
+    }
 
-	fn stop(&self) -> Vec<String> {
-		println!("Environment detail controller stopping");
-		vec![]
-	}
+    fn stop(&self) -> Vec<String> {
+        println!("Environment detail controller stopping");
+        vec![]
+    }
 }
 
 #[derive(Hash)]
 struct HomeController;
 
 impl Controller for HomeController {
-	fn params(&mut self, params: &RouteParams) -> Option<()> {
-		if params.data.name == "home" {
-			Some(())
-		} else {
-			None
-		}
-	}
+    fn params(&mut self, params: &RouteParams) -> Option<()> {
+        if params.data.name == "/home" {
+            Some(())
+        } else {
+            None
+        }
+    }
 
-	fn start(&self) -> Vec<String> {
-		println!("Home controller start!");
-		vec![format!("load the home screen")]
-	}
+    fn start(&self) -> Vec<String> {
+        println!("Home controller start!");
+        vec![format!("load the home screen")]
+    }
 }
 
-
-
-#[derive(Controller)]
-#[trigger(route("/environments"))]
-struct EnvListController {}
-
-#[derive(Controller)]
-#[trigger(route("/environments/:id"))]
-struct EnvDetailController {
-	#[hash]
-	id: String,
+pub struct GenericRoute<T> {
+    data: T,
+    path_pattern: String,
 }
 
-impl EnvDetailController {
-	const NAME: &'static str = "env_detail";
-}
-
-#[derive(Controller)]
-#[trigger(once)]
-struct UserBasicController;
-
-impl UserBasicController {
-	const NAME: &'static str = "user_basic";
-}
-
-
-
-
-#[route("/", "home")]
-struct HomeRoute;
-
-#[route("/login", "login")]
 #[derive(Debug)]
-struct LoginRoute;
-
-#[route("/users", "user_list")]
-struct UsersRoute;
-
-#[route("/environments", "env_list")]
-struct EnvironmentsRoute;
-
-#[route("/environments/new", "new_env")]
-struct NewEnvironmentRoute;
-
-#[route("/environments/:id/detail", "env_detail")]
-struct EnvironmentDetailRoute {
-	id: String,
+pub struct LimitOffsetQuery {
+    limit: Option<u64>,
+    offset: Option<u64>,
 }
 
-#[route("/environments/:id/logs", "env_logs")]
-struct EnvironmentLogsRoute {
-	id: String,
+#[derive(Debug)]
+pub struct SomeOtherQuery {
+    include_all: bool,
 }
 
-#[route("/environments/:id/vars", "env_vars")]
-struct EnvironmentVarsRoute {
-	id: String,
+pub trait AppPath {
+    fn path_pattern() -> String
+    where
+        Self: Sized;
+    fn from_str(app_path: &str) -> Option<Self>
+    where
+        Self: Sized;
+    fn query_string(&self) -> Option<String>;
+    fn to_string(&self) -> String;
 }
 
+#[derive(Debug)]
+pub enum SortDirection {
+    Asc,
+    Desc,
+}
 
+// #[route("/emails/{email_id}?{query}", "email_detail")]
+#[derive(AppPath)]
+// #[path("/emails/{email_id}")]
+// /p/{project_id}/exams/active?column=updated_at&direction=desc&keyword=test
+#[path("/p/{project_id}/exams/active")]
+struct ExamListPath {
+    project_id: String,
 
+    #[query]
+    column: Option<String>,
 
+    #[query]
+    direction: Option<SortDirection>,
 
+    #[query]
+    keyword: Option<String>,
+}
 
+#[derive(AppPath)]
+#[path("/settings/account/profile")]
+struct SelfProfilePath {}
 
+#[derive(AppPath, Debug)]
+#[path("/p/:project_id/exams/:exam_id/submissions_expired")]
+struct ExpiredSubmissionsPath {
+    project_id: String,
 
+    exam_id: u64,
 
+    #[query]
+    column: Option<String>,
 
+    #[query]
+    direction: Option<SortDirection>,
+
+    #[query]
+    keyword: Option<String>,
+}
+
+impl AppPath for ExpiredSubmissionsPath {
+    fn path_pattern() -> String {
+        r"^/p/(?P<project_id>[^/]+)/exams/(?P<exam_id>[^/]+)/submissions_expired$".to_string()
+    }
+
+    fn from_str(app_path: &str) -> Option<Self> {
+        let path_pattern = Regex::new(&Self::path_pattern()).ok()?;
+        let captures = path_pattern.captures(app_path)?;
+
+        // TODO - get query string
+
+        Some(ExpiredSubmissionsPath {
+            project_id: captures["project_id"].parse().ok()?,
+            exam_id: captures["exam_id"].parse().ok()?,
+            column: None,
+            direction: None,
+            keyword: None,
+        })
+    }
+
+    fn query_string(&self) -> Option<String> {
+        None
+    }
+
+    fn to_string(&self) -> String {
+        format!(
+            "/p/{}/exams/{}/submissions_expired",
+            self.project_id, self.exam_id
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct MyQuery {
+    name: String,
+
+    limit_offset: Option<LimitOffsetQuery>,
+}
 
 fn main() {
+    println!(
+        "Pattern of ExpiredSubmissionsPath: {}",
+        ExpiredSubmissionsPath::path_pattern()
+    );
+    let app_path_string = "/p/43/exams/10/submissions_expired";
+    let expired_path = ExpiredSubmissionsPath::from_str(app_path_string);
+
+    println!("expired_path: {:#?}", expired_path);
+    // println!("expired_path URL: {}", expired_path.unwrap().to_string());
+
+    let routes: Vec<Box<dyn AppPath>> = vec![Box::new(expired_path.unwrap())];
+
+    for route in routes {
+        println!("Route: {}", route.to_string());
+    }
+
     // let controllers: Vec<Box<dyn Controller>> = vec!(Box::new(EnvironmentDetailController::default()), Box::new(HomeController));
 
-    println!("EnvDetailController name: {}", EnvDetailController::NAME);
-    println!("UserBasicController name: {}", UserBasicController::NAME);
+    // let email_path = ExamListPath {
+    // 	email_id: "some_email".to_string(),
+    // 	limit_offset: Some(LimitOffsetQuery {
+    // 		limit: Some(20),
+    // 		offset: Some(20),
+    // 	}),
+    // 	some_other: SomeOtherQuery {
+    // 		include_all: false
+    // 	}
+    // };
 
-    println!("dfssadfd name: {:?}", LoginRoute {});
-
-    let stuff = EnvDetailController {
-        id: "".to_string(),
-    };
+    // println!("Query string: {}", email_path.query_string());
 
     let mut app = App::new();
     app.add_controller(EnvironmentDetailController::default());
     app.add_controller(HomeController);
 
-    app.new_route("environments/exam-copy".to_string());
-    app.new_route("environments/deadline-extension".to_string());
-    app.new_route("home".to_string());
-    app.new_route("home".to_string());
+    app.new_route("/environments/exam-copy".to_string());
+    app.new_route("/environments/deadline-extension".to_string());
+    app.new_route("/home".to_string());
+    app.new_route("/home".to_string());
 }
--- rs-frame-macros/src/lib.rs
+++ rs-frame-macros/src/lib.rs
@@ -1,34 +1,177 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
+use regex::Regex;
+use syn::{parse_macro_input, AttributeArgs, DeriveInput};
 
 // #[proc_macro]
 // pub fn example_macro_fn(_: TokenStream) -> TokenStream {
 // 	"struct Stuff;".parse().unwrap()
 // }
 
-#[proc_macro_derive(Controller, attributes(hash, route, trigger))]
-pub fn controller_derive(input: TokenStream) -> TokenStream {
-	// let args = input.clone();
-	// let args = parse_macro_input!(args as AttributeArgs);
-	let input = parse_macro_input!(input as DeriveInput);
+#[derive(Debug, PartialEq)]
+enum PathToRegexError {
+    MissingLeadingForwardSlash,
+    NonAsciiChars,
+    InvalidIdentifier(String),
+    InvalidTrailingSlash,
+}
 
+fn path_to_regex(path: &str) -> Result<String, PathToRegexError> {
+    enum ParseState {
+        Initial,
+        Static,
+        VarName(String),
+    };
 
-	// println!("args: {:#?}", args);
-	println!("input: {:#?}", input);
-	"".parse().unwrap()
+    if !path.is_ascii() {
+        return Err(PathToRegexError::NonAsciiChars);
+    }
+
+    let ident_regex = Regex::new(r"^[a-zA-Z][a-zA-Z0-9_]*$").unwrap();
+
+    let mut regex = "".to_string();
+    let mut parse_state = ParseState::Initial;
+
+    for byte in path.chars() {
+        match parse_state {
+            ParseState::Initial => {
+                if byte != '/' {
+                    return Err(PathToRegexError::MissingLeadingForwardSlash);
+                }
+
+                regex += "^/";
+
+                parse_state = ParseState::Static;
+            }
+            ParseState::Static => {
+                if byte == ':' {
+                    parse_state = ParseState::VarName("".to_string());
+                } else {
+                    regex.push(byte);
+                    parse_state = ParseState::Static;
+                }
+            }
+            ParseState::VarName(mut name) => {
+                if byte == '/' {
+                    // Validate 'name' as a Rust identifier
+                    if !ident_regex.is_match(&name) {
+                        println!("checking name: {}\tbad!", name);
+                        return Err(PathToRegexError::InvalidIdentifier(name));
+                    } else {
+                        println!("checking name: {}\tgood!", name);
+                    }
+
+                    regex += &format!("(?P<{}>[^/]+)/", name);
+                    parse_state = ParseState::Static;
+                } else {
+                    name.push(byte);
+                    parse_state = ParseState::VarName(name);
+                }
+            }
+        };
+    }
+
+    if let ParseState::VarName(name) = parse_state {
+        regex += &format!("(?P<{}>[^/]+)", name);
+    }
+
+    if regex.ends_with("/") {
+        return Err(PathToRegexError::InvalidTrailingSlash);
+    }
+
+    regex += "$";
+
+    Ok(regex)
+}
+
+#[test]
+fn test_path_to_regex() {
+    let regex = path_to_regex("/p/:project_id/exams/:exam_id/submissions_expired").unwrap();
+    assert_eq!(
+        regex,
+        r"^/p/(?P<project_id>[^/]+)/exams/(?P<exam_id>[^/]+)/submissions_expired$"
+    );
+}
+
+#[test]
+fn test_path_to_regex_no_path_params() {
+    let regex = path_to_regex("/p/exams/submissions_expired").unwrap();
+    assert_eq!(regex, r"^/p/exams/submissions_expired$");
+}
+
+#[test]
+fn test_path_to_regex_no_leading_slash() {
+    let regex = path_to_regex("p/exams/submissions_expired");
+    assert_eq!(regex, Err(PathToRegexError::MissingLeadingForwardSlash));
+}
+
+#[test]
+fn test_path_to_regex_non_ascii_chars() {
+    let regex = path_to_regex("🥖p🥖:project_id🥖exams🥖:exam_id🥖submissions_expired");
+    assert_eq!(regex, Err(PathToRegexError::NonAsciiChars));
+}
+
+#[test]
+fn test_path_to_regex_invalid_ident() {
+    let regex = path_to_regex("/p/:project_id/exams/:exam*ID/submissions_expired");
+    assert_eq!(
+        regex,
+        Err(PathToRegexError::InvalidIdentifier("exam*ID".to_string()))
+    );
+
+    let regex = path_to_regex("/p/:project_id/exams/:_exam_id/submissions_expired");
+    assert_eq!(
+        regex,
+        Err(PathToRegexError::InvalidIdentifier("_exam_id".to_string()))
+    );
+}
+
+#[test]
+fn test_path_to_regex_invalid_ending() {
+    let regex = path_to_regex("/p/:project_id/exams/:exam_id/submissions_expired/");
+    assert_eq!(regex, Err(PathToRegexError::InvalidTrailingSlash));
+}
+
+#[proc_macro_derive(AppPath, attributes(path, query))]
+pub fn app_path_derive(input: TokenStream) -> TokenStream {
+    println!("AppPath Struct:");
+
+    // let args = input.clone();
+    // let args = parse_macro_input!(args as AttributeArgs);
+    let input = parse_macro_input!(input as DeriveInput);
+
+    for attr in &input.attrs {
+        println!("attrs: {:#?}", attr);
+    }
+
+    // println!("args: {:#?}", args);
+    println!("input: {:#?}", input);
+    "".parse().unwrap()
 }
 
 #[proc_macro_attribute]
 pub fn route(args: TokenStream, input: TokenStream) -> TokenStream {
-	let out = input.clone();
-	// let args = input.clone();
-	// let args = parse_macro_input!(args as AttributeArgs);
-	let input = parse_macro_input!(input as DeriveInput);
+    println!("New Struct:");
 
+    // let out = input.clone();
+    // let args = input.clone();
+    // let args = parse_macro_input!(args as AttributeArgs);
+    let input = parse_macro_input!(input as DeriveInput);
 
-	println!("args: {:#?}", args);
-	println!("input: {:#?}", input);
-	
-	out
+    let args = parse_macro_input!(args as AttributeArgs);
+
+    println!("args: {:#?}", args);
+    // for thing in args {
+    // 	match thing {
+    // 		proc_macro::TokenTree::Literal(lit) => println!("lit is {}", lit),
+    // 		_ => {}
+    // 	}
+    // }
+
+    // println!("input: {:#?}", input);
+
+    // out
+    let output = quote! { #input };
+    output.into()
 }
